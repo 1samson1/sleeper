4ef4873d038b9d02dfe61a82dc5ff0d06d3012bf
1samson1 <ya.samsono2013@ya.ru>
2022-10-22 02:39:14 +0300

    update time and add timer

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QMessageBox>
 #include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -32,9 +33,80 @@ void MainWindow::resizeLogo()
     ui->logo->setPixmap(logo.scaled(w,h, Qt::KeepAspectRatio, Qt::SmoothTransformation));
 }
 
+void MainWindow::changeActiveBtn()
+{
+    if(ui->btn_start->isEnabled())
+    {
+        ui->box_mode->setEnabled(false);
+        ui->box_time->setEnabled(false);
+        ui->btn_start->setEnabled(false);
+        ui->btn_stop->setEnabled(true);
+    }
+    else {
+        ui->box_mode->setEnabled(true);
+        ui->box_time->setEnabled(true);
+        ui->btn_start->setEnabled(true);
+        ui->btn_stop->setEnabled(false);
+    }
+}
+
+int MainWindow::toTimeStamp(int seconds, int minutes, int hours)
+{
+    return 1000 * (seconds + minutes * 60 + hours * 3600);
+}
+
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_btn_start_clicked()
+{
+    changeActiveBtn();
+
+    connect(&_timer, &QTimer::timeout,
+            this, &MainWindow::timeout);
+    _timer.start(toTimeStamp(ui->seconds->value(),
+                             ui->minutes->value(),
+                             ui->hours->value()));
+
+    connect(&_time_updater, &QTimer::timeout,
+            this, &MainWindow::updateTime);
+
+    _time_updater.start(900); // Every 900 milliseconds update time in ui
+}
+
+
+void MainWindow::on_btn_stop_clicked()
+{
+    changeActiveBtn();
+    stopTimer();
+}
+
+void MainWindow::timeout()
+{
+    stopTimer();
+    QMessageBox::information(this, "Sleep", "Sleep");
+}
+
+void MainWindow::updateTime()
+{
+    int time = _timer.remainingTime() / 1000;
+    ui->seconds->setValue(time % 60); time /= 60;
+    ui->minutes->setValue(time % 60); time /= 60;
+    ui->hours->setValue(time);
+
+}
+
+void MainWindow::stopTimer()
+{
+    _timer.disconnect(&_timer, &QTimer::timeout,
+                      this, &MainWindow::timeout);
+    _timer.stop();
+
+    disconnect(&_time_updater, &QTimer::timeout,
+            this, &MainWindow::updateTime);
+    _time_updater.stop();
 }
 
 
--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -19,9 +20,22 @@ protected:
     virtual void showEvent(QShowEvent *event) override;
     virtual void resizeEvent(QResizeEvent *event) override;
 
+private slots:
+    void on_btn_start_clicked();
+    void on_btn_stop_clicked();
+
+    void timeout();
+    void updateTime();
+
 private:
     void resizeLogo();
+    void changeActiveBtn();
+    void stopTimer();
 
+    int toTimeStamp(int seconds, int minutes = 0, int hours = 0);
+
+    QTimer _timer;
+    QTimer _time_updater;
     Ui::MainWindow *ui;
 };
 
